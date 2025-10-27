cc3037a0fec178546fc144e181b7bc7ac6fe6ccc
Michele Dusi <michele.dusi@unibs.it>
2025-10-27 15:37:07 +0100

    Counting papers for every year

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import requests
 from xml.etree import ElementTree as ET
 
-YEARS_RANGE = range(2023, 2025)
-DBLP_SOURCES = [f"https://dblp.org/search/publ/api?q=toc%3Adb/conf/ecai/ecai{year}.bht%3A&h=1000&format=xml" for year in YEARS_RANGE]
+YEARS_RANGE = range(2000, 2025)
 
-
-for source in DBLP_SOURCES:
-    print(f"Fetching papers from: {source}")
+for year in YEARS_RANGE:
+    source = f"https://dblp.org/search/publ/api?q=toc%3Adb/conf/ecai/ecai{year}.bht%3A&h=1000&format=xml"
+    source_alt = f"https://dblp.org/search/publ/api?q=toc%3Adb/conf/ijcai/ijcai{year}.bht%3A&h=1000&format=xml"
+    print(f"Fetching papers published in {year} from: '{source}'")
     # Opening XML and reading 
     # Papers are represented as <hit> elements in the XML
     # <results> / <hits> / <hit>
 
     response = requests.get(source)
-    if response.status_code == 200:
-        xml_content = response.content
-        root = ET.fromstring(xml_content)
+    response_alt = requests.get(source_alt)
+    
+    xml_content = response.content
+    xml_content_alt = response_alt.content
 
-        hits = root.find('hits')
-        if hits is not None:
-            for hit in hits.findall('hit'):
-                info = hit.find('info')
-                if info is not None:
-                    authors = info.find('authors')
-                    if authors is not None:
-                        author_names = [author.text for author in authors.findall('author')]
-                        print("Authors:", author_names)
+    root = ET.fromstring(xml_content)
+    root_alt = ET.fromstring(xml_content_alt)
 
-                else:
-                    print("No info found for this hit.")
-        else:
-            print("No hits found in the XML.")
+    hits = root.find('hits')
+    hits_alt = root_alt.find('hits')
+
+    papers = hits.findall('hit') if hits is not None else []
+    papers_alt = hits_alt.findall('hit') if hits_alt is not None else []
+
+    if len(papers) == 0 and len(papers_alt) > 0:
+        print(f"No papers found for year {year} in primary source, switching to alternative source.")
+        papers = papers_alt
+
+    if len(papers) == 0:
+        print(f"No papers found for year {year} in both sources.")
+        continue
+    else:
+        print(f"Found {len(papers)} papers published in {year}.")
+
+        for hit in papers:
+            info = hit.find('info')
+            if info is not None:
+                authors = info.find('authors')
+                if authors is not None:
+                    author_names = [author.text for author in authors.findall('author')]
+
+            else:
+                print("No info found for this hit.")
