6245abc50c2c866db95b1e1873e95ab2b4880b71
friha438 <hartman.frida@gmail.com>
2025-10-27 16:52:10 +0200

    up to date

--- save_to_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def names_to_csv(data):
-    with open("authors.csv", mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["id", "year", "author_name", "nr_of_instances"])
-        writer.writeheader()
-        writer.writerows(data)
